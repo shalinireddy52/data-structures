171c340e549c7feb3cbe1a8c5b5ef746dbf348b5
Shalini <bodeshalinireddy@gmail.com>
2023-03-13 12:00:00 -0500

    Created a singly linked list structure with basic node operations

---
+++ linkedlist/linkedlist.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.value, end=" -> ")
+            current = current.next
+        print("None")
