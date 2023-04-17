30ff1941aec3c1b552fae86980b57f1b75cf447c
Shalini <bodeshalinireddy@gmail.com>
2023-04-17 12:00:00 -0500

    Built a queue using an array with enqueue and dequeue operations

---
+++ Queue/Queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.queue.pop(0)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def peek(self):
+        return self.queue[0] if self.queue else None
