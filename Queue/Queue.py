a4f3ba7572b6fe15b3ccfeef32605d9769f17a43
Shalini <bodeshalinireddy@gmail.com>
2023-08-17 12:00:00 -0500

    queue enhancement

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,17 +1,48 @@
 class Queue:
-    def __init__(self):
-        self.queue = []
+    def __init__(self, initial_capacity=2):
+        self.queue = [None] * initial_capacity
+        self.front = 0
+        self.rear = 0
+        self.size = 0
+        self.capacity = initial_capacity
 
     def enqueue(self, value):
-        self.queue.append(value)
+        if self.size == self.capacity:
+            self._resize(self.capacity * 2)
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.capacity
+        self.size += 1
 
     def dequeue(self):
         if self.is_empty():
-            return None
-        return self.queue.pop(0)
-
-    def is_empty(self):
-        return len(self.queue) == 0
+            raise IndexError("Dequeue from empty queue")
+        value = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        if self.size <= self.capacity // 4 and self.capacity > 2:
+            self._resize(self.capacity // 2)
+        return value
 
     def peek(self):
-        return self.queue[0] if self.queue else None
+        if self.is_empty():
+            raise IndexError("Peek from empty queue")
+        return self.queue[self.front]
+
+    def is_empty(self):
+        return self.size == 0
+
+    def _resize(self, new_capacity):
+        new_queue = [None] * new_capacity
+        for i in range(self.size):
+            new_queue[i] = self.queue[(self.front + i) % self.capacity]
+        self.front = 0
+        self.rear = self.size
+        self.queue = new_queue
+        self.capacity = new_capacity
+
+    def clear(self):
+        self.queue = [None] * self.capacity
+        self.front = self.rear = self.size = 0
+
+    def display(self):
+        return [self.queue[(self.front + i) % self.capacity] for i in range(self.size)]
